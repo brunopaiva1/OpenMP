9239d91df6e28bf715db974701fc8ef9d5dfda4e
brunopaiva1 <brunopaivamaster10@gmail.com>
2023-03-29 17:07:37 -0300

    three commit

--- Hello/omp_hello.c
+++ Hello/omp_hello.c
@@ -9,23 +9,23 @@ void Hello(void);  /* Função Thread */
 
 /*--------------------------------------------------------------------*/
 int main(int argc, char* argv[]) {
-   int thread_count = strtol(argv[1], NULL, 10); 
+   int thread_count = strtol(argv[1], NULL, 10); /* Função Strtol para obter o número de threads*/
 
-#  pragma omp parallel num_threads(thread_count) 
+#  pragma omp parallel num_threads(thread_count) /* Diretiva para paralelização do número de threads*/
    Hello();
-
+// A cada interação é feita a chamada para a função Hello
    return 0; 
 }  /* main */
 
 /*-------------------------------------------------------------------
- * Function:    Hello
- * Purpose:     Thread function that prints message
+ * Função:    Hello
+ * Propósito:     Função da thread que imprime a mensagem
  */
 void Hello(void) {
-#  ifdef _OPENMP   
+#  ifdef _OPENMP   /* Verificação se _OPENMP foi validado*/
       int my_rank = omp_get_thread_num();
       int thread_count = omp_get_num_threads();
-#  else
+#  else /* Caso não seja teremos uma thread única*/
       int my_rank = 0;
       int thread_count = 1;
 #  endif
